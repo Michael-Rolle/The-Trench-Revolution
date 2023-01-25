d21962f75197f8999cb45c3a8f6de9eec72e8a81
Michael Rolle <michaelrolle7@gmail.com>
2023-01-25 19:46:41 +0200

    Added UnitController class

---
+++ game-source-code/UnitController.cpp
@@ -0,0 +1,95 @@
+#include "UnitController.h"
+
+UnitController::UnitController(): \
+    friendlyUnits{},
+    enemyUnits{}
+{
+    totalTime = 0;
+    if(!enemyRiflemanText.loadFromFile("resources/Soldier-Guy-PNG/_Mode-Gun/01-Idle/E_E_Gun__Idle_000.png"))
+        throw "Cannot load texture";
+}
+
+void UnitController::addFriendlyUnit(shared_ptr<Unit> unit)
+{
+    friendlyUnits.push_back(unit);
+    sort(friendlyUnits.begin(), friendlyUnits.end()); //sorts them based on the row they're in
+}
+
+void UnitController::addEnemyUnit(shared_ptr<Unit> unit)
+{
+    enemyUnits.push_back(unit);
+    sort(enemyUnits.begin(), enemyUnits.end());
+}
+
+void UnitController::updateUnits(const float deltaTime, const float gameWidth, const float gameHeight)
+{
+    totalTime += deltaTime;
+    for(auto& unit : friendlyUnits)
+    {
+        if(unit->reloading)
+            unit->reload(deltaTime);
+        if(unit->getPositionX() < (0.01*gameWidth)*unit->blockNum)
+        {
+            unit->advance(deltaTime);
+        }
+        else//Check for enemies in range
+        {
+            if(unit->blockNum < 70)
+                unit->blockNum++;
+        }
+        unit->fire(enemyUnits);
+        if(!unit->alive)
+        {
+            std::remove(friendlyUnits.begin(), friendlyUnits.end(), unit);
+            friendlyUnits.erase(friendlyUnits.end());
+            break;
+        }
+
+    }
+
+    if(totalTime > 5+(rand()%10))
+    {
+        totalTime = 0;
+        UnitController::addEnemyUnit(make_shared<Rifleman>(&enemyRiflemanText, gameWidth, gameHeight, false));
+    }
+
+    for(auto& unit: enemyUnits)
+    {
+        if(unit->reloading)
+            unit->reload(deltaTime);
+        if(unit->getPositionX() > (0.01*gameWidth)*unit->blockNum)
+        {
+            unit->advance(deltaTime);
+        }
+        else
+        {
+            if(unit->blockNum > 10)
+                unit->blockNum--;
+        }
+        unit->fire(friendlyUnits);
+        if(!unit->alive)
+        {
+            std::remove(enemyUnits.begin(), enemyUnits.end(), unit);
+            enemyUnits.erase(enemyUnits.end());
+            break;
+        }
+
+    }
+}
+
+void UnitController::draw(sf::RenderWindow& window, const GameState gameState)
+{
+    if(gameState == GameState::Playing)
+    {
+        for(auto& unit : friendlyUnits)
+        {
+            unit->draw(window, gameState);
+        }
+        for(auto& unit : enemyUnits)
+        {
+            unit->draw(window, gameState);
+        }
+    }
+}
+
+
---
+++ game-source-code/UnitController.h
@@ -0,0 +1,29 @@
+#ifndef UNITCONTROLLER_H
+#define UNITCONTROLLER_H
+
+#include "Unit.h"
+#include "Drawable.h"
+#include "Rifleman.h"
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+using namespace std;
+
+class UnitController : public Drawable
+{
+    public:
+        UnitController();
+        void addFriendlyUnit(shared_ptr<Unit> unit);
+        void addEnemyUnit(shared_ptr<Unit> unit);
+        void updateUnits(const float deltaTime, const float gameWidth, const float gameHeight); //must involve removing dead units
+        virtual void draw(sf::RenderWindow& window, const GameState gameState) override;
+
+    private:
+        float totalTime;
+        sf::Texture enemyRiflemanText;
+        vector<shared_ptr<Unit>> friendlyUnits;
+        vector<shared_ptr<Unit>> enemyUnits;
+};
+
+#endif // UNITCONTROLLER_H
